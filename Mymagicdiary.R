c6346265411a3affdfc8dc9b282e84056e76ba69
Flaviotto <130578210+Flaviotto@users.noreply.github.com>
2023-04-12 17:24:29 +0200

    Create Mymagicdiary.R

---
+++ Mymagicdiary.R
@@ -0,0 +1,14 @@
+# function()
+sherika <- c(10, 50, 70, 90, 100)
+alessio <- c(5, 10, 20, 40, 80)
+# plot
+# higher dimension
+plot(alessio, sherika, cex=5, col="red", pch=19)
+# colour
+# colour character
+# labels
+plot(alessio, sherika, cex=5, col="red", pch=19, xlab="gonorrhea bacterium", cex.label=2, main="gonorrea")
+
+# correlation
+cor(alessio, sherika)
+
